b93de01177ab64e276cb600eaf7faa3c6bf2cae2
root <root@ip-172-31-19-64.ap-northeast-2.compute.internal>
2021-03-26 00:18:17 +0900

    update tr1

--- Training/tr1.go
+++ Training/tr1.go
@@ -2,9 +2,53 @@ package main
 
 import(
 	"fmt"
-	"math/rand"
+	"math"
 )
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(math.Pi)
+	fmt.Println(ret1(1,5))
+
+	a, b := ret2("world", "hello")
+	fmt.Println(a, b)
+
+	var c, d = ret3(15)
+	fmt.Println(c, d)
+
+	var e, f int = 1, 2
+	fmt.Println(e, f)
+
+	var tr, fls = true, false
+	fmt.Println(tr, fls)
+}
+
+// add
+func ret1(a ,b int) int {
+	return a+b
+}
+
+// swap
+func ret2(a, b string) (string, string) {
+	return b, a
+}
+
+// naked return
+func ret3(sum int) (x, y int) {
+	x = sum * 2 / 4
+	y = sum - x
+	// return (x, y)
+	return
+}
+
+
+func types() (x bool) {
+	/*
+		bool string int int8 int16 int32 int64 uint uint8 uint16 uint32 uint64 uintptr 
+		byte // alias for uint8 
+		rune // alias for int32 // represents a Unicode code point 
+		float32 float64 complex64 complex128
+	*/
+	x = false
+	return 
 }
